99c19bcb6361246623c350788ec6b589c6484ae8
chaturvedi.aishwarya <chaturvedi.aishwarya@nissiagency.com>
2018-10-30 15:41:21 +0530

    Connect to mongodb and create book schema

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema/schema')
+const mongoose = require('mongoose')
 
 const server = express()
+
+// connect to mlab
+mongoose.connect('mongodb://aish:test1234@ds145463.mlab.com:45463/gql-1', { useNewUrlParser: true })
+mongoose.connection.once('open', () => {
+  console.log('connected to db gql-1')
+})
 
 server.use('/graphql', graphqlHTTP({
   schema,
